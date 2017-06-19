788f616f1ed8e95b818724761779aebe983c19a8
Yohannes Tamru <yohannist@gmail.com>
2017-06-19 16:16:28 +0300

    add bamboo_browse and bamboo_results_endpoints configurations

--- Slackbot/config.py
+++ Slackbot/config.py
@@ -1,4 +1,6 @@
-import re, json, os
+import re
+import json
+import os
 from enum import Enum
 
 build_key_pattern = re.compile(r'(build) (S2S|EMS)-(\d\d\d\d?)', re.IGNORECASE)
@@ -12,14 +14,16 @@ bamboo_queue_build_endpoint = ''
 bamboo_uid = ''
 bamboo_pwd = ''
 cache_location = ''
+bamboo_results_endpoint = ''
+bamboo_browse = ''
 
 initialized = False
 
 def init():
-    global bot_name, token, bot_id, bot_sleep_delay    ,bamboo_plans_endpoint    ,bamboo_queue_build_endpoint    ,bamboo_uid    ,bamboo_pwd,cache_location
+    global bot_name, token, bot_id, bot_sleep_delay, bamboo_plans_endpoint, bamboo_queue_build_endpoint,bamboo_uid, bamboo_pwd, cache_location, bamboo_results_endpoint, bamboo_browse
 
-    settings_file = open('./settings.json');
-    settings = json.loads(settings_file.read());
+    settings_file = open('./settings.json')
+    settings = json.loads(settings_file.read())
     settings_file.close()
 
     bot_name = settings['slack']['botName']
@@ -27,8 +31,10 @@ def init():
     bot_id = settings['slack']['id']
     bot_sleep_delay = settings['slack']['botNapTime']
 
-    bamboo_plans_endpoint =  settings['bamboo']['plansEndpoint']
+    bamboo_plans_endpoint = settings['bamboo']['plansEndpoint']
     bamboo_queue_build_endpoint = settings['bamboo']['queueBuildEndpoint']
+    bamboo_results_endpoint = settings['bamboo']['resultsEndpoint']
+    bamboo_browse = settings['bamboo']['bambooBrowse']
 
     bamboo_uid = settings['bamboo']['auth']['uid']
     bamboo_pwd = settings['bamboo']['auth']['pwd']
