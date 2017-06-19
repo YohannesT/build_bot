32ef28f237e79593b5489b5b60dd403a3aea105e
Yohannes Tamru <yohannist@gmail.com>
2017-06-19 16:06:33 +0300

    check for None on messageid

--- Slackbot/messageid.py
+++ Slackbot/messageid.py
@@ -1,16 +1,31 @@
 import config
+
 def is_hi(message):
+    if message is None:
+        return False
     tokens = [word.lower() for word in message.strip().split()]
     return any(g in tokens for g in ['hello', 'bonjour', 'hey', 'hi', 'sup', 'morning', 'gm', 'good morning', 'hola', 'yo', 'ሰላም', 'ሀይ'])
 
 def is_bye(message):
+    if message is None:
+        return False
     tokens = [word.lower() for word in message.strip().split()]
     return any(g in tokens for g in ['bye', 'goodbye', 'revoir', 'adios', 'ቻው'])
 
 def is_help(message):
+    if message is None:
+        return False
     tokens = [word.lower() for word in message.strip().split()]
     return any(g in tokens for g in ['help', '--help'])
 
 def is_build_request(message):
+    if message is None:
+        return False
     result = config.build_key_pattern.search(message)
     return result is not None and len(result.group()) > 0 
+
+def is_dont_run(message):
+    if message is None:
+        return False
+    tokens = [word.lower() for word in message.strip().split()]
+    return any(g in tokens for g in ['stop', 'dont', 'don\'t', 'halt'])
